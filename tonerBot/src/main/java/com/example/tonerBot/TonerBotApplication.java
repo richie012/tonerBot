1c5cc232d4f8db1b81e631a526663a8df3a5eecc
richie <82734398+richie012@users.noreply.github.com>
2022-12-14 22:52:23 +0500

    Add files via upload

---
+++ tonerBot/src/main/java/com/example/tonerBot/TonerBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.tonerBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonerBotApplication.class, args);
+	}
+
+}
